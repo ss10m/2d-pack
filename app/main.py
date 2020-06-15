d97310d64655e88d9b3bcbb84b0c11a058fce807
ss <fastf20@gmail.com>
2020-06-15 11:32:42 -0400

    readded react

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+#app = Flask(__name__)
+app = Flask(__name__, static_folder="./build/static", template_folder="./build")
 
-@app.route("/")
-def hello():
-    return "Hello World from Flask23"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index_orderr(path):
+    print(path)
+    return render_template('index.html')
