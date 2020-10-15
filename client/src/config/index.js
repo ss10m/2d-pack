15daaa9b389063d84f3faed8fea98bbdb910b010
ss <fastf20@gmail.com>
2020-10-15 14:26:07 -0400

    changed url

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,6 @@
 const API_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8080"
-        : "https://ship.ssprojects.ca";
+        : "https://pack.ssprojects.ca";
 
 export { API_URL };
